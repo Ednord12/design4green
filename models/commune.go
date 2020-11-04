c30816080d2c739994c02dc6132cbc508057254b
Meven <meven.turmel@gmail.com>
2020-11-04 17:04:10 +0100

    Modèles

--- models/commune.go
+++ models/commune.go
@@ -1,4 +1,26 @@
 package models
 
+import "github.com/astaxie/beego/orm"
+
 type Commune struct {
+	ID               int    `json:"id" orm:"auto;pk;column(id)"`
+	NomIris          string `json:"nom_iris" orm:"column(nom_iris)"`
+	CodePostal       string `json:"code_postal" orm:"column(code_postal)"`
+	NomCommune       string `json:"nom_commune" orm:"column(nom_commune)"`
+	ScoreGlobal      int    `json:"score_global" orm:"column(score_global)"`
+	ScoreAccesIntnum int    `json:"score_acces_intnum" orm:"column(score_acces_intnum)"`
+	ScoreAccesInfo   int    `json:"score_acces_info" orm:"column(score_acces_info)"`
+	ScoreCompAdmin   int    `json:"score_comp_admin" orm:"column(score_comp_admin)"`
+	ScoreCompNumsco  int    `json:"score_comp_numsco" orm:"column(score_comp_numsco)"`
+	RegionId         int    `json:"region_id" orm:"column(region_id)"`
+	DepartementId    int    `json:"departement_id" orm:"column(departement_id)"`
+}
+
+func init() {
+	orm.RegisterModel(new(Commune))
+}
+
+// TableName : table Commune
+func (c *Commune) TableName() string {
+	return "Commune"
 }
--- models/region.go
+++ models/region.go
@@ -1,4 +1,18 @@
 package models
 
+import "github.com/astaxie/beego/orm"
+
 type Region struct {
+	ID          int    `json:"id" orm:"auto;pk;column(id)"`
+	Nom         string `json:"nom" orm:"column(nom)"`
+	ScoreGlobal int    `json:"score_global" orm:"column(score_global)"`
+}
+
+func init() {
+	orm.RegisterModel(new(Region))
+}
+
+// TableName : table Region
+func (c *Region) TableName() string {
+	return "Region"
 }
